7afea87d989fbd972ce429ab543601e928425206
arslanakhtar61 <arslanakhtar61@gmail.com>
2017-05-01 15:14:29 +0800

    Update countX.java

--- countX.java
+++ countX.java
@@ -6,3 +6,16 @@ countX("xhixhix") → 3
 countX("hi") → 0
 */
 
+	public int countX(String str) {
+		  
+		  if(str.length() == 1 || str.length() == 0 ){
+		    if(str.equals("x")) return 1;
+		    return 0;
+		  }
+		  else{
+		    if((str.charAt(str.length()-1) == 'x')) return 1 + countX(str.substring(0, str.length()-1));
+		    return 0 + countX(str.substring(0, str.length()-1));
+		  }
+		  
+		}
+
