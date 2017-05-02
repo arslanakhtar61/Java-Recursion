21604c3c295321c1db2edf765f76505850fc06f2
arslanakhtar61 <arslanakhtar61@gmail.com>
2017-05-02 21:56:47 +0800

    Create stringClean.java

---
+++ stringClean.java
@@ -0,0 +1,13 @@
+/*
+Given a string, return recursively a "cleaned" string where adjacent chars that are the same have been reduced to a single char. So "yyzzza" yields "yza".
+
+stringClean("yyzzza") → "yza"
+stringClean("abbbcdd") → "abcd"
+stringClean("Hello") → "Helo"
+*/
+
+public String stringClean(String str) {
+  if(str.length() <= 1) return str;
+  if(str.substring(0, 1).equals(str.substring(1, 2))) return "" + stringClean(str.substring(1));
+  return str.substring(0, 1) + stringClean(str.substring(1));
+}
